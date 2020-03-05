46e1b71be48b4cbe1e4aa66e11e927017eb99cee
Håkon <>
2020-03-05 10:36:03 +0100

    Gjort ferdig oppgave 2, feil tolkning(?)

--- task2.py
+++ task2.py
@@ -1,12 +1,18 @@
 from pyspark import SparkContext
 from operator import add
 from datetime import datetime
+import numpy as np
 
 l_file = [
-    "./yelp-data/yelp_businesses.csv"
-    ,"./yelp-data/yelp_top_reviewers_with_reviews.csv"
-    ,"./yelp-data/yelp_top_users_friendship_graph.csv"
+    "yelp_businesses.csv"
+    ,"yelp_top_reviewers_with_reviews.csv"
+    ,"yelp_top_users_friendship_graph.csv"
     ]
+
+def PCC(x,y,X,Y):
+    num = (x-X)*(y-Y)
+    den = np.sqrt((x-X)**2)*np.sqrt((y-Y)**2)
+    return num/den
 
 def load_data(context,url):
     data = context.textFile(url)
@@ -41,7 +47,7 @@ def get_reviews_year(data):
     lines = data.map(lambda s: (get_year(s.split("\t")[4]),1))
     rev_year = lines.reduceByKey(lambda a,b: a+b)
     sorted_rev = rev_year.sortBy(lambda x: x[0])
-    r_list = rev_year.collect()
+    r_list = sorted_rev.collect()
     for year in r_list:
         print(year)
     return r_list
@@ -57,19 +63,52 @@ def get_first_last(data):
     print(get_timestring(last))
 
 def get_amount_reviews(data):
-    #(user_id, num_reviews)
+    '''
+    (user_id, num_reviews)
+    '''
+    lines = data.map(lambda s: (s.split("\t")[1],1))
+    user_reviews = lines.reduceByKey(lambda a,b: a+b)
+    print('Number of user reviews first: {}'.format(user_reviews.first()))
+    return user_reviews
+
 
 def get_average_reviews(data):
-    #average_reviews
+    '''
+    (avg_reviews)
+    '''
+    user_reviews = get_amount_reviews(data)
+    avg_reviews = user_reviews.values().mean()
+    print('Average reviews: {0}'.format(avg_reviews))
+    return avg_reviews
 
 def get_avg_lenreview(data):
-    #(user_id, avg_len)
+    '''
+    (user_id,avg_len(reviews))
+    '''
+    lines = data.map(lambda s: (s.split("\t")[1],len(s.split("\t")[3])))
+    mappedLines = lines.mapValues(lambda v: (v,1))
+    reducedLines = mappedLines.reduceByKey(lambda a,b: (a[0]+b[0], a[1]+b[1]))
+    avg_by_key = reducedLines.mapValues(lambda v: v[0]/v[1])
+    print('Average by key first: {0}'.format(avg_by_key.first()))
+    return avg_by_key
 
 def get_avg_len_total(data):
     #average_length
-    
-def final_func(data)
+    avg_len = data.map(lambda s: len(s.split("\t")[3])).mean()
+    print('Average length: {0}'.format(avg_len))
+    return avg_len
+
+def final_func(data):
     #(userid, rating)
+    Y = get_avg_len_total(data)
+    X = get_average_reviews(data)
+    avg_lenreview = get_avg_lenreview(data)
+    user_reviews = get_amount_reviews(data)
+    user_avg = avg_lenreview.join(user_reviews)
+    print('Final func user_avg first: {0}'.format(user_avg.first()))
+    user_rating = user_avg.mapValues(lambda v: PCC(v[1],v[0],X,Y))
+    print('Final func user_rating first: {0}'.format(user_rating.first()))
+    return user_rating
 
 def get_year(uni_stamp):
     return int(datetime.fromtimestamp(float(uni_stamp)).year)
@@ -79,8 +118,13 @@ def get_timestring(uni_stamp):
 
 context = SparkContext("local", "first app")
 data = load_data(context,l_file[1])
-get_reviews_year(data)
+#get_reviews_year(data)
 #get_first_last(data)
+#get_avg_lenreview(data)
+#get_average_reviews(data)
+#get_average_reviews(data)
+#get_avg_len_total(data)
+final_func(data)
 '''
 for url in l_file:
     data = context.textFile(url)
